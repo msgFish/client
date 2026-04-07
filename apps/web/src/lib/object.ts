39f911085aa55381b74262376fdbd3f2142c3226
duskette <dev@duskette.net>
2026-04-06 20:05:10 -0400

    woahhhhhhhh

--- apps/web/src/lib/object.ts
+++
@@ -1,3 +0,0 @@
-export function ojoin(...obj: object[]) {
-  return Object.assign({}, ...obj);
-}
--- apps/web/src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import './index.css';
-
-  const { children } = $props();
-</script>
-
-{@render children?.()}
