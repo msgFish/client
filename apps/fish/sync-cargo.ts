0b3f8ce9097eb586e6d80c3bfdaca7f5356c5bac
duskette <dev@duskette.net>
2026-04-10 08:45:58 -0400

    ugh windows c'mon man

--- apps/fish/sync-cargo.ts
+++ apps/fish/sync-cargo.ts
@@ -1,30 +1,45 @@
 import fs from 'node:fs';
 import pkg from './package.json';
 
-const CARGO_TOML_PATH = "src-tauri/Cargo.toml";
+const CARGO_CONFIG_PATH = "src-tauri/Cargo.toml";
+const TAURI_CONFIG_PATH = "src-tauri/tauri.config.json";
 
-const cargoToml = fs.readFileSync(CARGO_TOML_PATH, "utf-8");
-let version = pkg.version;
+const cargoConfig = fs.readFileSync(CARGO_CONFIG_PATH, "utf-8");
+const semver = pkg.version;
 
+const updatedCargo = cargoConfig.replace(
+  /^version\s*=\s*".*"/m,
+  `version = "${semver}"`
+);
+
+fs.writeFileSync(CARGO_CONFIG_PATH, updatedCargo);
+
+/* Handle Windows because it's special */
 const isWindows = process.env.BUILD_TARGET?.includes("windows");
+
+if (isWindows) {
+  const tauriConfig = JSON.parse(fs.readFileSync(TAURI_CONFIG_PATH, "utf-8"));
+
+  tauriConfig.bundle = tauriConfig.bundle || {};
+  tauriConfig.bundle.windows = tauriConfig.bundle.windows || {};
+  tauriConfig.bundle.windows.wix = tauriConfig.bundle.windows.wix || {};
+
+  const semverWindows = toMsiVersion(semver);
+
+  tauriConfig.bundle.windows.wix.version = semverWindows;
+
+  fs.writeFileSync(
+    TAURI_CONFIG_PATH, 
+    JSON.stringify(tauriConfig, null, 2)
+  );
+}
 
 function toMsiVersion(version: string) {
   const match = version.match(/^(\d+)\.(\d+)\.(\d+)(?:-.*?(\d+))?/);
-  if (!match) return version;
+  if (!match) return '0.0.0';
 
   const [, major, minor, patch, pre] = match;
   return pre
     ? `${major}.${minor}.${patch}.${pre}`
     : `${major}.${minor}.${patch}`;
 }
-
-if (isWindows) {
-  version = toMsiVersion(version);
-}
-
-const updated = cargoToml.replace(
-  /^version\s*=\s*".*"/m,
-  `version = "${version}"`
-);
-
-fs.writeFileSync(CARGO_TOML_PATH, updated);
